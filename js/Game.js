5667bd5cb8bd1d02780c8fe6965fa6cb4e3baf5f
Mateusz Zerebiec <43859749+ZerebiecMateusz@users.noreply.github.com>
2023-10-11 20:33:57 +0200

    explode enemy

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,18 @@
 import { Spaceship } from "./Spaceship.js";
+import { Enemy } from "./Enemy.js";
 
 class Game {
     #htmlElements = {
         spaceship: document.querySelector('[data-spaceship]'),
         container: document.querySelector('[data-container]')
     }
-    #ship = new Spaceship(this.#htmlElements.spaceship, this.#htmlElements.container)
-    #checkPositionInterval = null;
+    #ship = new Spaceship(
+        this.#htmlElements.spaceship, 
+        this.#htmlElements.container);
+				#enemies = [];
+				#enemiesInterval = null;
+    		#checkPositionInterval = null;
+    		#createEnemyInterval = null;
     
     init() {
         this.#ship.init();
@@ -14,10 +20,42 @@ class Game {
     }
 
     #newGame() {
+				this.#enemiesInterval = 30;
         this.#checkPositionInterval = setInterval(() => this.#checkPosition(), 1)
+				this.#createEnemyInterval = setInterval(() => this.#randomNewEnemy(), 1000)
     }
 
+		#randomNewEnemy() {
+			const randomNumber = Math.floor(Math.random() * 5) + 1;
+					randomNumber % 5 ? this.#createNewEnemy(
+					this.#htmlElements.container, 
+					this.#enemiesInterval, 'enemy', 'explosion') : this.#createNewEnemy(
+						this.#htmlElements.container, 
+						this.#enemiesInterval * 2, 
+						'enemy--big', 'explosion--big',	3,
+						);
+		}
+
+		#createNewEnemy(...params) {
+			const enemy = new Enemy(
+				...params,
+			);
+			enemy.init();
+			this.#enemies.push(enemy)
+	}
+
     #checkPosition() {
+			this.#enemies.forEach((enemy, enemyIndex, enemiesArr) => {
+				const enemyPosition = {
+						top: enemy.element.offsetTop,
+						right: enemy.element.offsetLeft + enemy.element.offsetWidth,
+						bottom: enemy.element.offsetTop + enemy.element.offsetHeight,
+						left: enemy.element.offsetLeft
+				}
+				if (enemyPosition.top > window.innerHeight) {
+						enemy.explode();
+						enemiesArr.splice(enemyIndex, 1)
+				}
         this.#ship.missiles.forEach((missile, missileIndex, missileArr) => {
             const missilePosition = {
                 top: missile.element.offsetTop,
@@ -25,11 +63,20 @@ class Game {
                 bottom: missile.element.offsetTop + missile.element.offsetHeight,
                 left: missile.element.offsetLeft
             }
+						if(missilePosition.bottom >= enemyPosition.top && missilePosition.top <= enemyPosition.bottom && missilePosition.right >= enemyPosition.left && missilePosition.left <= enemyPosition.right) {
+							enemy.hit();
+							if (!enemy.lives) {
+								enemiesArr.splice(enemyIndex, 1);
+							}
+							missile.remove();
+							missileArr.splice(missileIndex, 1)
+						}
             if (missilePosition.bottom < 0) {
                 missile.remove();
                 missileArr.splice(missileIndex, 1)
             }
         })
+			})
     }
 }
 
--- js/Spaceship.js
+++ js/Spaceship.js
@@ -46,10 +46,10 @@ export class Spaceship {
         requestAnimationFrame(this.#gameLoop)
     }
     #whatKey() {
-        if(this.#leftArrow && this.#getPosition() > 0) {
+        if(this.#leftArrow && this.#getPosition() > 12) {
             this.element.style.left = `${parseInt(this.element.style.left, 10) - this.#modifier}px`;
         }
-        if(this.#rightArrow && this.#getPosition() < window.innerWidth) {
+        if(this.#rightArrow && this.#getPosition() + 12 < window.innerWidth) {
             this.element.style.left = `${parseInt(this.element.style.left, 10) + this.#modifier}px`;
         }
     }
